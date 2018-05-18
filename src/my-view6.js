506a2ccdbc49ee19bbab9e0bce361d3eda50bb6d
Parvati Menon <pnaliyat@iu.edu>
2018-05-18 00:24:42 -0400

    added code to remove if the fields are undefined on receiving data and reduced if else to few lines of code

--- src/my-view6.js
+++ src/my-view6.js
@@ -12,69 +12,54 @@ import '@polymer/iron-ajax/iron-ajax.js';
 class MyView6 extends LitElement {
     _didRender() {
         this.shadowRoot.getElementById('ajax').addEventListener('response', function(e){
-
-            if((e.detail.response.maritalStatus === undefined) || (e.detail.response.maritalStatus = UNK))
+            if(e.detail.response.maritalStatus != undefined)
             {
-                e.target.parentNode.getElementById('unknown').checked = true;
-            }
-            else if(e.detail.response.maritalStatus = A)
+            var i=0;
+            while(true)
+            {
+                if(e.target.parentNode.querySelectorAll("mwc-radio")[i].value == e.detail.response.maritalStatus)
                 {
-                    e.target.parentNode.getElementById('Annulled').checked = true;
+                    e.target.parentNode.querySelectorAll("mwc-radio")[i].checked = true;
+                    break;
+                }
+                i++;
             }
-            else if(e.detail.response.maritalStatus = D)
+            }
+            else if(e.detail.response.maritalStatus == undefined)
             {
-                e.target.parentNode.getElementById('divorced').checked = true;
+                e.target.parentNode.removeChild(e.target.parentNode.childNodes[2]);
             }
-            else if(e.detail.response.maritalStatus = I)
-            {
-                e.target.parentNode.getElementById('interlocutory').checked = true;
-            }
-            else if(e.detail.response.maritalStatus = L)
-            {
-                e.target.parentNode.getElementById('legallyseparated').checked = true;
-            }
-            else if(e.detail.response.maritalStatus = M)
-            {
-                e.target.parentNode.getElementById('married').checked = true;
-            }
-            else if(e.detail.response.maritalStatus = P)
-            {
-                e.target.parentNode.getElementById('polygamous').checked = true;
-            }
-            else if(e.detail.response.maritalStatus = S)
-            {
-                e.target.parentNode.getElementById('nevermarried').checked = true;
-            }
-            else if(e.detail.response.maritalStatus = T)
-            {
-                e.target.parentNode.getElementById('domesticpartner').checked = true;
-            }
-            else if(e.detail.response.maritalStatus = U)
-            {
-                e.target.parentNode.getElementById('unmarried').checked = true;
-            }
-            else if(e.detail.response.maritalStatus = W)
-            {
-                e.target.parentNode.getElementById('widowed').checked = true;
-            }
-            //console.log(e.target.parentNode.getElementById('widowed').value);
         });
     }
 
     _render({}) {
         return html`
-       <mwc-formfield label="Annulled"><mwc-radio id="annulled" value="A"></mwc-radio></mwc-formfield>
-       <mwc-formfield label="Divorced"><mwc-radio id="divorced" value="D"></mwc-radio></mwc-formfield><br>
-       <mwc-formfield label="Interlocutory"><mwc-radio id="interlocutory" value="I"></mwc-radio></mwc-formfield>            
-       <mwc-formfield label="Legally Separated"><mwc-radio id="legallyseparated" value="L"></mwc-radio></mwc-formfield><br>
-       <mwc-formfield label="Married"><mwc-radio id="married" value="M"></mwc-radio></mwc-formfield>
-       <mwc-formfield label="Polygamous"><mwc-radio id="polygamous" value="P"></mwc-radio></mwc-formfield><br>
-       <mwc-formfield label="Never Married"><mwc-radio id="nevermarried" value="S"></mwc-radio></mwc-formfield>             
-       <mwc-formfield label="Domestic partner"><mwc-radio id="domesticpartner" value="T"></mwc-radio></mwc-formfield><br>
-       <mwc-formfield label="unmarried"><mwc-radio id="unmarried" value="U"></mwc-radio></mwc-formfield>
-       <mwc-formfield label="Widowed"><mwc-radio id="widowed" value="W"></mwc-radio></mwc-formfield><br>            
-       <mwc-formfield label="Unknown"><mwc-radio id="unknown" value="UNK"></mwc-radio></mwc-formfield>
+       <table class="tabletable-responsive">
+       <form>
+       <tr>
+       <td><mwc-formfield label="Annulled"><mwc-radio id="annulled" value="A"></mwc-radio></mwc-formfield></td>
+       <td><mwc-formfield label="Divorced"><mwc-radio id="divorced" value="D"></mwc-radio></mwc-formfield></td>
+       <td><mwc-formfield label="Interlocutory"><mwc-radio id="interlocutory" value="I"></mwc-radio></mwc-formfield></td>          
+       <td><mwc-formfield label="Legally Separated"><mwc-radio id="legallyseparated" value="L"></mwc-radio></mwc-formfield></td>
+       <td><mwc-formfield label="Widowed"><mwc-radio id="widowed" value="W"></mwc-radio></mwc-formfield></td>
+       <td><mwc-formfield label="Unmarried"><mwc-radio id="unmarried" value="U"></mwc-radio></mwc-formfield></td>
+       </tr>
+       <tr>
+       <td><mwc-formfield label="Married"><mwc-radio id="married" value="M"></mwc-radio></mwc-formfield></td>
+       <td><mwc-formfield label="Polygamous"><mwc-radio id="polygamous" value="P"></mwc-radio></mwc-formfield></td>
+       <td><mwc-formfield label="Never Married"><mwc-radio id="nevermarried" value="S"></mwc-radio></mwc-formfield></td>             
+       <td><mwc-formfield label="Domestic partner"><mwc-radio id="domesticpartner" value="T"></mwc-radio></mwc-formfield></td>
+        <td><mwc-formfield label="Unknown"><mwc-radio id="unknown" value="UNK"></mwc-radio></mwc-formfield></td> 
+       </tr>
+       <tr>
+       
+      
+       
+       </tr> 
+       
        <iron-ajax id="ajax" auto handle-as="json" url="http://hapi.fhir.org/baseDstu3/Patient/2"></iron-ajax> 
+       </form>
+       </table>
     `;
     }
 }
