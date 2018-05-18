c1fbd51402146302f549723cada8a2635936d6c3
Parvati Menon <pnaliyat@iu.edu>
2018-05-17 20:51:33 -0400

    completed active status with removal in case of empty data

--- src/my-view2.js
+++ src/my-view2.js
@@ -5,30 +5,31 @@ import '@material/mwc-checkbox/mwc-checkbox.js'
 import '@polymer/iron-ajax/iron-ajax.js';
 class MyView2 extends LitElement {
     _didRender() {
+        
         this.shadowRoot.getElementById('ajax').addEventListener('response', function(e){
-            if(e.detail.response.active = false)
+            if(e.detail.response.active)
+            {
+                e.target.parentNode.getElementById('active').checked = true;
+            }
+            else if(!e.detail.response.active)
             {
                 e.target.parentNode.getElementById('active').checked = false;
-
             }
-            else{
-                e.target.parentNode.getElementById('active').checked = true;
-
+            else
+            {
+                e.target.parentNode.removeChild(e.target.parentNode.childNodes[1]);
             }
-            // "e.detail.response.active" is check box status from server
-            //"e.target.parentNode.getElementById('check')".checked is at the user side checkbox
+
         });
     }
-
     _render({}) {
         return html`
        <mwc-formfield alignEnd label="Active status:">
-         <mwc-checkbox id="active"></mwc-checkbox>
+         <mwc-checkbox id="active" value="true"></mwc-checkbox>
          </mwc-formfield>
-       <iron-ajax id="ajax" auto handle-as="json" url="http://hapi.fhir.org/baseDstu3/Patient/3629129"></iron-ajax>
+       <iron-ajax id="ajax" auto handle-as="json" url="http://hapi.fhir.org/baseDstu3/Patient/2"></iron-ajax>
        
     `;
     }
 }
-
 window.customElements.define('my-view2', MyView2);
