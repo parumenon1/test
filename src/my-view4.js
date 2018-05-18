c329ae2f23fd192341a48bbbdedca4a2853173e1
Parvati Menon <pnaliyat@iu.edu>
2018-05-17 20:11:12 -0400

    completed patient geneder without if else

--- src/my-view4.js
+++ src/my-view4.js
@@ -7,37 +7,32 @@ import '@polymer/iron-ajax/iron-ajax.js';
 class MyView4 extends LitElement {
     _didRender() {
         this.shadowRoot.getElementById('ajax').addEventListener('response', function(e){
-            if(e.detail.response.gender = 'female')
+            var i=0;
+            while(true)
             {
-                e.target.parentNode.getElementById('female').checked = true;
+                if(e.target.parentNode.querySelectorAll("mwc-radio")[i].value == e.detail.response.gender)
+                {
+                    e.target.parentNode.querySelectorAll("mwc-radio")[i].checked = true;
+                    break;
+                }
+                i++;
+            }
+            if(e.detail.response.gender == undefined)
+            {
+                var child = e.target.parentNode.querySelectorAll("mwc-radio");
+                e.target.parentNode.removeChild(child);
 
             }
-            else if(e.detail.response.gender = 'male')
-            {
-                e.target.parentNode.getElementById('male').checked = true;
 
-            }
-            else if(e.detail.response.gender = 'other')
-            {
-                e.target.parentNode.getElementById('other').checked = true;
-
-
-            }
-            else if(e.detail.response.gender = 'unknown')
-            {
-                e.target.parentNode.getElementById('unknown').checked = true;
-
-            }
-            console.log(e.detail.response.gender.value);
         });
     }
     _render({}) {
         return html`
-       <mwc-formfield label="Male"><mwc-radio id="male" value="male"></mwc-radio></mwc-formfield><br>
-         <mwc-formfield label="Female"><mwc-radio id="female" value="female"></mwc-radio></mwc-formfield><br>
-         <mwc-formfield label="Other"><mwc-radio id="other" value="other"></mwc-radio> </mwc-formfield> <br>            
-         <mwc-formfield label="Unknown"><mwc-radio id="unknown" value="unknown"></mwc-radio></mwc-formfield>
-       <iron-ajax id="ajax" auto handle-as="json" url="http://hapi.fhir.org/baseDstu3/Patient/3629128"></iron-ajax>
+       <mwc-formfield label="Male"><mwc-radio name="gender" id="male" value="male"></mwc-radio></mwc-formfield><br>
+         <mwc-formfield label="Female"><mwc-radio name="gender" id="female" value="female"></mwc-radio></mwc-formfield><br>
+         <mwc-formfield label="Other"><mwc-radio name="gender" id="other" value="other"></mwc-radio> </mwc-formfield> <br>            
+         <mwc-formfield label="Unknown"><mwc-radio name="gender" id="unknown" value="unknown"></mwc-radio></mwc-formfield>
+       <iron-ajax id="ajax" auto handle-as="json" url="http://hapi.fhir.org/baseDstu3/Patient/3665782"></iron-ajax>
        
     `;
     }
