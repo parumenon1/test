c8364fe5204567cd7c2fe8e120ffaa91dd81094f
Parvati Menon <pnaliyat@iu.edu>
2018-05-21 21:42:52 -0400

    completed deceased

--- src/my-view3.js
+++ src/my-view3.js
@@ -1,50 +1,46 @@
-//PATIENT DECEASED STATUS--what about datetime of deceased????
+//PATIENT DECEASED STATUS
 import {LitElement, html} from '@polymer/lit-element/lit-element.js';
-
 import '@material/mwc-checkbox/mwc-checkbox.js'
 import '@material/mwc-formfield/mwc-formfield.js';
 import '@polymer/iron-ajax/iron-ajax.js';
+import './fhir-period.js';
 
 class MyView3 extends LitElement {
+    static get properties() {
+        return {
+            deceaseStatus: Boolean,
+            periodField: Boolean,
+            url: String
+        }
+    }
+
+    constructor() {
+        super();
+        this.deceaseStatus = true;
+        this.periodField = false;
+    }
     _didRender() {
-        this.shadowRoot.getElementById('ajax').addEventListener('response', function(e){
-            if(true)
-            {
-                if (e.detail.response.deceasedBoolean) {
-                    e.target.parentNode.getElementById('decease').checked = true;
-                }
-                else if (!e.detail.response.deceasedBoolean) {
-                    e.target.parentNode.getElementById('decease').checked = false;
-                }
-                else {
-                    e.target.parentNode.removeChild(e.target.parentNode.childNodes[1]);
-                    e.target.parentNode.removeChild(e.target.parentNode.childNodes[3]);
-                }
+        this.shadowRoot.getElementById('ajax').addEventListener('iron-ajax-response', function (e) {
+            if (e.detail.response.deceasedBoolean) {
+                e.target.parentNode.getElementById('decease').checked = true;
             }
-            if (e.detail.response.deceasedDateTime != undefined)
+            else (!e.detail.response.deceasedBoolean)
             {
-                e.detail.response.deceasedDateTime = e.target.parentNode.getElementById('date');
-            }
-            else{
-                e.target.parentNode.removeChild(e.target.parentNode.childNodes[3]);
+                e.target.parentNode.getElementById('decease').checked = false;
             }
 
-            // "e.detail.response.active" is check box status from server
-            //"e.target.parentNode.getElementById('check')".checked is at the user side checkbox
         });
     }
 
-    _render({}) {
+    _render({deceaseStatus, periodField, url}) {
         return html`
-      <mwc-formfield alignEnd label="Deceased status:">
+      ${deceaseStatus ? html`<mwc-formfield class="deceaseStatus" alignEnd label="Deceased status:">
          <mwc-checkbox id="decease" value = "true"></mwc-checkbox>
-      </mwc-formfield> 
-      <mwc-formfield alignEnd label="Deceased date:">
-         <input id="date" type="datetime-local">
-       </mwc-formfield>      
-      <iron-ajax id="ajax" auto handle-as="json" url="http://hapi.fhir.org/baseDstu3/Patient/2"></iron-ajax> 
-       
-    `;
+      </mwc-formfield>` : ''}
+      ${periodField ? html`<fhir-period class="periodField"></fhir-period>`: ''}     
+          <iron-ajax id="ajax" auto handle-as="json" url="${url}"></iron-ajax> 
+        
+`;
     }
 }
 
