34823b929fa3d9f80b0dd78cbb56198782e51721
Parvati Menon <pnaliyat@iu.edu>
2018-05-22 17:03:41 -0400

    wip lang

--- src/my-view9.js
+++ src/my-view9.js
@@ -1,19 +1,52 @@
-//PATIENT IDENTIFIER
+//PATIENT LANG
 import {LitElement, html} from '@polymer/lit-element/lit-element.js';
 import '@material/mwc-textfield/mwc-textfield.js';
+import '@material/mwc-switch/mwc-switch.js'
 import '@polymer/iron-ajax/iron-ajax.js';
 class MyView9 extends LitElement {
+    static get properties() {
+        return {
+            langField: Boolean,
+            prefField: Boolean,
+            url: String
+        }
+    }
+
+    constructor() {
+        super();
+        this.langField = true;
+        this.prefField = false;
+    }
+
     _didRender() {
         this.shadowRoot.getElementById('ajax').addEventListener('response', function(e){
-            e.target.parentNode.getElementById('identifier').value = e.detail.response.id;
-
+            if(e.detail.response.communication != undefined) {
+                var i = 0;
+                for (let identifier of e.detail.response.identifier) {
+                    if (i > 0) {
+                        var child = e.target.parentNode.childNodes[1].cloneNode(true);
+                        e.target.parentNode.appendChild(child);
+                    }
+                    e.target.parentNode.querySelectorAll('.langField').value = e.detail.response.communication.language;
+                    i++;
+                    //can add receiving boolean value of preference if developer decides to use it
+                }
+            }
+                else if(e.detail.response.communication == undefined)
+               {
+                e.target.parentNode.removeChild(e.target.parentNode.childNodes[1]);
+               }
         });
     }
 
-    _render({}) {
+    _render({langField, prefField, url}) {
         return html`
-     <mwc-textfield outlined id="identifier" label="Patient ID:"></mwc-textfield>
-     <iron-ajax id="ajax" auto handle-as="json" url="http://hapi.fhir.org/baseDstu3/Patient/2"></iron-ajax>
+     <div>
+     ${langField ? html`<mwc-textfield outlined class="langField" label="Language"></mwc-textfield>` : ''}
+     ${prefField ? html`<mwc-formfield alignEnd>Language preferred:<mwc-switch id ="pref" class="prefField" value="true"></mwc-switch></mwc-formfield>` : ''}
+     </div>
+     <iron-ajax id="ajax" auto handle-as="json" url="${url}"></iron-ajax>
+     
     `;
     }
 }
