e1baadbe041669c2e40f2b79aa247f3888992659
Parvati Menon <pnaliyat@iu.edu>
2018-05-21 21:55:33 -0400

    completed gender

--- src/my-view4.js
+++ src/my-view4.js
@@ -5,34 +5,42 @@ import '@material/mwc-radio/mwc-radio.js';
 import '@polymer/iron-ajax/iron-ajax.js';
 
 class MyView4 extends LitElement {
+    static get properties() {
+        return {
+            genderVal: Boolean,
+            url: String
+        }
+    }
+
+    constructor() {
+        super();
+        this.genderVal = true;
+
+    }
+
     _didRender() {
-        this.shadowRoot.getElementById('ajax').addEventListener('response', function(e){
-            var i=0;
-            while(true)
-            {
-                if(e.target.parentNode.querySelectorAll("mwc-radio")[i].value == e.detail.response.gender)
-                {
+        this.shadowRoot.getElementById('ajax').addEventListener('iron-ajax-response', function (e) {
+            var i = 0;
+            while (true) {
+                if (e.target.parentNode.querySelectorAll("mwc-radio")[i].value == e.detail.response.gender) {
                     e.target.parentNode.querySelectorAll("mwc-radio")[i].checked = true;
                     break;
                 }
                 i++;
             }
-            if(e.detail.response.gender == undefined)
-            {
-                var child = e.target.parentNode.querySelectorAll("mwc-radio");
-                e.target.parentNode.removeChild(child);
-
-            }
-
         });
     }
-    _render({}) {
+
+    _render({genderVal, url}) {
         return html`
-       <mwc-formfield label="Male"><mwc-radio name="gender" id="male" value="male"></mwc-radio></mwc-formfield><br>
+       ${genderVal ? html`
+       <div class="genderVal">
+         <mwc-formfield label="Male"><mwc-radio name="gender" id="male" value="male"></mwc-radio></mwc-formfield><br>
          <mwc-formfield label="Female"><mwc-radio name="gender" id="female" value="female"></mwc-radio></mwc-formfield><br>
          <mwc-formfield label="Other"><mwc-radio name="gender" id="other" value="other"></mwc-radio> </mwc-formfield> <br>            
          <mwc-formfield label="Unknown"><mwc-radio name="gender" id="unknown" value="unknown"></mwc-radio></mwc-formfield>
-       <iron-ajax id="ajax" auto handle-as="json" url="http://hapi.fhir.org/baseDstu3/Patient/3665782"></iron-ajax>
+       </div>` : ''}
+       <iron-ajax id="ajax" auto handle-as="json" url="${url}"></iron-ajax>
        
     `;
     }
