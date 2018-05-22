0b41685fc1906461fe72e20980d4a64ec26b79af
Parvati Menon <pnaliyat@iu.edu>
2018-05-22 18:06:35 -0400

    wip address

--- src/my-view8.js
+++ src/my-view8.js
@@ -1,51 +1,80 @@
-//PATIENT ADDRESS???label of date not showing up
+//PATIENT ADDRESS
 import {LitElement, html} from '@polymer/lit-element/lit-element.js';
 import '@material/mwc-textfield/mwc-textfield.js';
 import '@polymer/iron-ajax/iron-ajax.js';
+import './fhir-period.js';
 class MyView8 extends LitElement {
+    static get properties() {
+        return {
+            useSelect: Boolean,
+            typeSelect: Boolean,
+            add1Field: Boolean,
+            add2Field: Boolean,
+            cityField: Boolean,
+            districtField: Boolean,
+            periodField: Boolean,
+            url: String
+        }
+    }
+
+    constructor() {
+        super();
+        this.useSelect = true;
+        this.typeSelect = true;
+        this.add1Field = true;
+        this.add2Field = true;
+        this.cityField = true;
+        this.districtField = true;
+        this.periodField = false;
+    }
     _didRender() {
-        this.shadowRoot.getElementById('ajax').addEventListener('response', function(e){
+        this.shadowRoot.getElementById('ajax').addEventListener('iron-ajax-response', function(e){
             if(e.detail.response.address != undefined)
-            {
-
-                //add options value for use and type; e.target.parentNode5,10 for use and type resp
-                e.target.parentNode.getElementById('city').value =  e.detail.response.address[0].city;
-                e.target.parentNode.getElementById('district').value = e.detail.response.address[0].district;
-                e.target.parentNode.getElementById('sdate').value = e.detail.response.address[0].period.start;
-                e.target.parentNode.getElementById('edate').value = e.detail.response.address[0].period.end;
-                e.target.parentNode.getElementById('address1').value = e.detail.response.address[0].text;
-                e.target.parentNode.getElementById('address2').value = e.detail.response.address[0].line;
+            {var i = 0;
+                for (let address of e.detail.response.address) {
+                    if (i > 0) {
+                        var child = e.target.parentNode.childNodes[1].cloneNode(true);
+                        e.target.parentNode.appendChild(child);
+                    }
+                    e.target.parentNode.querySelectorAll('.useSelect')[i].value =  e.detail.response.address[0].use;
+                    e.target.parentNode.querySelectorAll('.typeSelect')[i].value = e.detail.response.address[0].type;
+                    e.target.parentNode.querySelectorAll('.add1Field')[i].value = e.detail.response.address[0].text;
+                    e.target.parentNode.querySelectorAll('.add2Field')[i].value = e.detail.response.address[0].line;
+                    e.target.parentNode.querySelectorAll('.cityField')[i].value = e.detail.response.address[0].city;
+                    e.target.parentNode.querySelectorAll('.districtField')[i].value = e.detail.response.address[0].district;
+                    i++;
+                }
             }
-            else if(e.detail.response.address == undefined) {
-                //remove all child nodes of e.target.parent
-
+            else if(e.detail.response.address == undefined)
+            {
+                e.target.parentNode.removeChild(e.target.parentNode.childNodes[1]);
             }
         });
     }
-    _render({}) {
+    _render({useSelect, typeSelect, add1Field, add2Field, cityField, districtField, periodField, url}) {
         return html`
-     <h1>ADDRESS1</h1> 
-     <label>Use</label>
-     <select>
+     <div> 
+     ${useSelect ? html`
+     Use:<select class="useSelect">
          <option value="home">Home</option>
          <option value="work">Work</option>
          <option value="temp">Temporary</option>
          <option value="old">Old</option>
-     </select><br>
-     <label>Type</label>
-     <select>
-         <option value="home">Postal</option>
-         <option value="work">Physical</option>
-         <option value="temp">Both</option>
-     </select><br>
-     <mwc-textfield outlined id="address1" label="Address Line1:"></mwc-textfield><br>
-     <mwc-textfield outlined id="address2" label="Address Line2:"></mwc-textfield><br>
-     <mwc-textfield outlined id="city" label="City:"></mwc-textfield>
-     <mwc-textfield outlined id="district" label="District:"></mwc-textfield><br>
-     <mwc-formfield alignEnd label="start date:"><input id="sdate" type="date"></mwc-formfield>
-     <mwc-formfield alignEnd label="end date:"><input id="edate" type="date"></mwc-formfield>
-     <iron-ajax id="ajax" auto handle-as="json" url="http://hapi.fhir.org/baseDstu3/Patient/3709976"></iron-ajax>
-    `;
+     </select><br>` : ''}
+     ${typeSelect ? html`
+     Type:<select class="typeSelect">
+         <option value="postal">Postal</option>
+         <option value="physical">Physical</option>
+         <option value="both">Both</option>
+     </select><br>` : ''}
+     ${add1Field ? html`<mwc-textfield outlined class="add1Field" id="address1" label="Address Line1:"></mwc-textfield><br>` : ''}
+     ${add2Field ? html`<mwc-textfield outlined class="add2Field"  id="address2" label="Address Line2:"></mwc-textfield><br>` : ''}
+     ${cityField ? html`<mwc-textfield outlined class="cityField" id="city" label="City:"></mwc-textfield>` : ''}
+     ${districtField ? html`<mwc-textfield outlined class="districtField" id="district" label="District:"></mwc-textfield><br>` : ''}
+     ${periodField ? html`<fhir-period class="periodField"></fhir-period>` : ''}
+     </div>
+         <iron-ajax id="ajax" auto handle-as="json" url="${url}"></iron-ajax>
+`;
     }
 }
 window.customElements.define('my-view8', MyView8);
