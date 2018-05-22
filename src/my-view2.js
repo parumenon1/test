3e9104190f9216d99f69c22567777ddb803269f1
Parvati Menon <pnaliyat@iu.edu>
2018-05-22 14:52:17 -0400

    wip

--- src/my-view2.js
+++ src/my-view2.js
@@ -3,6 +3,7 @@ import {LitElement, html} from '@polymer/lit-element/lit-element.js';
 import '@material/mwc-formfield/mwc-formfield.js';
 import '@material/mwc-checkbox/mwc-checkbox.js'
 import '@polymer/iron-ajax/iron-ajax.js';
+
 class MyView2 extends LitElement {
     static get properties() {
         return {
@@ -10,33 +11,43 @@ class MyView2 extends LitElement {
             url: String
         }
     }
+
     constructor() {
         super();
         this.activeStatus = true;
 
     }
+
     _didRender() {
 
-        this.shadowRoot.getElementById('ajax').addEventListener('iron-ajax-response', function(e){
-            if(e.detail.response.active)
-            {
-                e.target.parentNode.getElementById('active').checked = true;
+        this.shadowRoot.getElementById('ajax').addEventListener('iron-ajax-response', function (e) {
+            if (e.detail.response.active != undefined) {
+                if (e.detail.response.active) {
+                    e.target.parentNode.querySelectorAll('.active').checked = true;
+                }
+                else (!e.detail.response.active)
+                {
+                    e.target.parentNode.querySelectorAll('.active').checked = false;
+                }
             }
-            else(!e.detail.response.active)
-            {
-                e.target.parentNode.getElementById('active').checked = false;
+            else if (e.detail.response.active == undefined) {
+                e.target.parentNode.removeChild(e.target.parentNode.childNodes[1]);
             }
 
         });
     }
+
     _render({activeStatus, url}) {
         return html`
+       <div>
        ${activeStatus ? html`<mwc-formfield class="activeStatus" alignEnd label="Active status:">
-         <mwc-checkbox id="active" value="true"></mwc-checkbox>
+         <mwc-checkbox class="active" value="true"></mwc-checkbox>
          </mwc-formfield>` : ''}
        <iron-ajax id="ajax" auto handle-as="json" url="${url}"></iron-ajax>
+       </div>
        
     `;
     }
 }
+
 window.customElements.define('my-view2', MyView2);
