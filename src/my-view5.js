11253f263d6da9dc0a78311b0206a7eb46c02931
Parvati Menon <pnaliyat@iu.edu>
2018-05-21 22:05:08 -0400

    wip address

--- src/my-view5.js
+++ src/my-view5.js
@@ -3,22 +3,28 @@ import {LitElement, html} from '@polymer/lit-element/lit-element.js';
 import '@material/mwc-formfield/mwc-formfield.js';
 import '@polymer/iron-ajax/iron-ajax.js';
 class MyView5 extends LitElement {
+    static get properties() {
+        return {
+            birthDate: Boolean,
+            url: String
+        }
+    }
+    constructor() {
+        super();
+        this.birthDate = true;
+    }
     _didRender() {
-        this.shadowRoot.getElementById('ajax').addEventListener('response', function(e){
+        this.shadowRoot.getElementById('ajax').addEventListener('iron-ajax-response', function(e){
             e.target.parentNode.getElementById('date').value = e.detail.response.birthDate;
-            if (e.detail.response.birthDate == undefined)
-            {
-                e.target.parentNode.removeChild(e.target.parentNode.childNodes[1]);
-            }
 
         });
     }
-    _render({}) {
+    _render({birthDate, url}) {
         return html`
-       <mwc-formfield alignEnd label="Date of Birth:">
+       ${birthDate ? html`<mwc-formfield class="birthDate" alignEnd label="Date of Birth:">
          <input id="date" type="date">
-       </mwc-formfield>
-       <iron-ajax id="ajax" auto handle-as="json" url="http://hapi.fhir.org/baseDstu3/Patient/3629128"></iron-ajax>
+       </mwc-formfield>` : ''}
+       <iron-ajax id="ajax" auto handle-as="json" url="${url}"></iron-ajax>
        
        
     `;
