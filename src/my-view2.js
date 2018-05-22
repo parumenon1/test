513e9df922496c297980661dfe204b26b1b8dae5
Parvati Menon <pnaliyat@iu.edu>
2018-05-21 21:21:22 -0400

    completed active status

--- src/my-view2.js
+++ src/my-view2.js
@@ -4,30 +4,37 @@ import '@material/mwc-formfield/mwc-formfield.js';
 import '@material/mwc-checkbox/mwc-checkbox.js'
 import '@polymer/iron-ajax/iron-ajax.js';
 class MyView2 extends LitElement {
+    static get properties() {
+        return {
+            activeStatus: Boolean,
+            url: String
+        }
+    }
+    constructor() {
+        super();
+        this.activeStatus = true;
+
+    }
     _didRender() {
-        
+
         this.shadowRoot.getElementById('ajax').addEventListener('response', function(e){
             if(e.detail.response.active)
             {
                 e.target.parentNode.getElementById('active').checked = true;
             }
-            else if(!e.detail.response.active)
+            else(!e.detail.response.active)
             {
                 e.target.parentNode.getElementById('active').checked = false;
-            }
-            else
-            {
-                e.target.parentNode.removeChild(e.target.parentNode.childNodes[1]);
             }
 
         });
     }
-    _render({}) {
+    _render({activeStatus}) {
         return html`
-       <mwc-formfield alignEnd label="Active status:">
+       ${activeStatus ? html`<mwc-formfield class="activeStatus" alignEnd label="Active status:">
          <mwc-checkbox id="active" value="true"></mwc-checkbox>
-         </mwc-formfield>
-       <iron-ajax id="ajax" auto handle-as="json" url="http://hapi.fhir.org/baseDstu3/Patient/2"></iron-ajax>
+         </mwc-formfield>` : ''}
+       <iron-ajax id="ajax" auto handle-as="json" url="{{url}}"></iron-ajax>
        
     `;
     }
